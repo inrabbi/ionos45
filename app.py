5f551822297296c920b46030c3951d288f55710a
rabbicraft <rabbicraft@outlook.com>
2024-09-27 14:35:20 -0700

    hello

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ app.secret_key = 'your_secret_key'  # Replace with your secret key
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'bv.engex@gmail.com '  # Your Gmail address
-app.config['MAIL_PASSWORD'] = 'zddccfulfddckuux'   # Your Gmail password or app-specific password
+app.config['MAIL_USERNAME'] = 'farahfaiez5@gmail.com '  # Your Gmail address
+app.config['MAIL_PASSWORD'] = 'qcbzxpassqxggwu'   # Your Gmail password or app-specific password
 
 mail = Mail(app)
 
